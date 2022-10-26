550d5b4643f3e915674dfb70163601519fd9c147
ShawnHan <hanzhuchen666@gmail.com>
2022-10-26 01:46:49 +0000

    update compiled, not debug yet

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include "SimulatorBase.hpp"
 #include <iostream>
+#include <cstring>
 
 
 int main(){
-    Dot* dot =new Dot(3,1,2,3);
-    std::cout<<dot<<std::endl;
-    Simulator sim = Simulator();
-    sim.addDot(dot);
-    std::cout<<(sim.dots[0]);
-    std::cout<<sim.dots[0]->m;
-    return 0;
+
+    int n_dot = 8;
+    int n_spring = n_dot * (n_dot - 1)/2;
+
+    Simulator sim = Simulator(0.001, 1000000);
+    
+    sim.addDot(1.0 ,0,0,0);
+    sim.addDot(1,1,0,0);
+    sim.addDot(1,0,1,0);
+    sim.addDot(1,1,1,0);
+    sim.addDot(1,0,0,1);
+    sim.addDot(1,0,1,1);
+    sim.addDot(1,1,0,1);
+    sim.addDot(1,1,1,1);
+    
+    for(int i =0; i< n_spring; ++i){
+        sim.addSpring(1.0 ,1.0 );
+        sim.deltaPos.push_back(new double[4]);
+    }
+
+    
 }
